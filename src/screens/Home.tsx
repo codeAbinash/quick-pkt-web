096ba50fa143400a9f9023841deb37bf4c4120d2
Abinash Karmakar <codeAbinash@gmail.com>
2023-09-14 21:28:42 +0530

    Added border radius

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -17,19 +17,7 @@ const Home = () => {
         <p className='text-center text-4xl font-semibold'>
           Welcome to
           <img src={images.logo_long} className='mx-auto w-60 pb-5 pt-5' />
-          {/* <span className='text-accent'>
-            <br /> Quick PKT{' '}
-          </span> */}
         </p>
-        {/* <p className='text-3xl font-medium'>
-          <span className='text-accent'>Hey</span> Champ
-          <span className='pl-1.5'>
-            <TextEmoji emoji='👋🏻' />
-          </span>
-        </p> */}
-        {/* <p className='text-lg'>
-          Welcome to <span className='text-accent'>Quick PKT</span>
-        </p> */}
         <p className='text-center'>Recharge, Relax, and Redeem Rewards!</p>
       </div>
 
@@ -39,7 +27,7 @@ const Home = () => {
         </p>
         <div className='flex w-full items-center justify-center rounded-btn bg-inputBg pl-4 text-btn dark:bg-white/10'>
           <p className='pr-1'>+91</p>
-          <div className=' darkL mx-2 h-[1.5em] w-0.5 bg-gray-300 dark:bg-white/20'></div>
+          <div className=' darkL mx-2 h-[1.5em] w-0.5 rounded-full bg-gray-300 dark:bg-white/20'></div>
           <input
             type='tel'
             className='w-full border-none bg-transparent p-4 pl-1 caret-accent outline-none'
