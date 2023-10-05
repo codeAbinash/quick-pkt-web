71f0571d7b5fa1d5d2028b3e16b61cad41cd3214
Abinash Karmakar <codeAbinash@gmail.com>
2023-10-05 16:28:00 +0530

    Showing version in profile screen from `app.tsx`

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 const app = {
   name: 'Quick Pkt',
   version: '1.0.0',
+  versionCode: 1,
   description: 'A quick way to Recharge',
   loginDescription: 'A quick way to Recharge',
   base: '',
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -11,6 +11,7 @@ import transitions from '../../lib/transition';
 import { blank_fn } from '../../lib/util';
 import { UserProfile } from './utils';
 import TapMotion from '../../components/TapMotion';
+import app from '../../../app';
 
 type Option = {
   name: string;
@@ -170,8 +171,8 @@ export default function Profile() {
                     option.link
                       ? transitions(() => navigate(option.link!))
                       : option.onClick
-                        ? transitions(() => option.onClick!())
-                        : blank_fn
+                      ? transitions(() => option.onClick!())
+                      : blank_fn
                   }
                 >
                   <div className='flex w-full items-center justify-between gap-6'>
@@ -179,8 +180,9 @@ export default function Profile() {
                       <img
                         src={option.icon}
                         alt=''
-                        className={`aspect-square w-5.5 opacity-80 dark:opacity-90 dark:invert ${option.classNameIcon ? option.classNameIcon : ''
-                          }`}
+                        className={`aspect-square w-5.5 opacity-80 dark:opacity-90 dark:invert ${
+                          option.classNameIcon ? option.classNameIcon : ''
+                        }`}
                       />
                       <span
                         className={`text-[0.85rem] font-420 opacity-90 ${option.className ? option.className : ''}`}
@@ -200,8 +202,10 @@ export default function Profile() {
             </div>
           </div>
         ))}
+        <p className='py-4 text-center text-xs font-normMid opacity-50 dark:opacity-30'>
+          {app.name} v{app.version} ({app.versionCode})
+        </p>
       </div>
-      <p className='text-center py-4 opacity-50 text-xs dark:opacity-30 '>Quick PKT v10.0.0(233)</p>
       <Watermark />
     </div>
   );
