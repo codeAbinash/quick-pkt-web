519fcb121aaedaaa837009b4e3bb1ca1b8a136d5
Abinash Karmakar <codeAbinash@gmail.com>
2023-10-14 13:49:58 +0530

    Updated emojis

--- src/components/TextEmoji.tsx
+++ src/components/TextEmoji.tsx
@@ -1,31 +1,11 @@
-import Emoji from 'emoji-store';
-let e_apple = new Emoji({
-  author: 'apple',
-  size: 160,
-  type: 'png',
-});
-
-let e_facebook = new Emoji({
-  author: 'facebook',
-  size: 96,
-  type: 'png',
-});
-
-type EmojiType = {
-  emoji: string;
-  type?: 'apple' | 'facebook';
-};
-
-export default function TextEmoji({ emoji, type }: EmojiType) {
-  let em = e_apple;
-  emoji = parseEmoji(emoji)[0];
-  if (type == 'facebook') em = e_facebook;
-  return <img src={em.get(emoji)} loading='lazy' className='inline-block h-[1.3em] align-middle' />;
-}
+import emoji from 'emoji-store';
 
 export function parseEmoji(emoji: string) {
   if (!emoji) return [''];
   // @ts-ignore
   let emojis = [...new Intl.Segmenter().segment(emoji)].map((x) => x.segment);
   return emojis;
+}
+export default function Emoji({ emoji: e = '😍' }) {
+  return <img src={emoji(e)} loading='lazy' className='inline-block h-[1.3em] align-middle' />;
 }
